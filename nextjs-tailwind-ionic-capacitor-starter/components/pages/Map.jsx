f9ee46ba45107c3a2004f44ab05908b21637750e
Zain <zainnmirzaa@gmail.com>
2023-10-16 20:12:03 -0500

    geolocation api

--- nextjs-tailwind-ionic-capacitor-starter/components/pages/Map.jsx
+++ nextjs-tailwind-ionic-capacitor-starter/components/pages/Map.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { IonPage, IonContent, IonTabButton } from '@ionic/react';
-import { terminal } from 'ionicons/icons';
-import handleClick from './nav';
-import GeoLocation from './nav';
-import location from './nav'
-
+import GeoLocation from './nav'; 
 function Map() {
 
-  return (
-    <IonPage>
-      <IonContent>
-        <IonTabButton id="share" onClick={GeoLocation()}>
-          Share My Location
-        </IonTabButton>
-      </IonContent>
-    </IonPage>
-  );
+    var location = GeoLocation(); 
+    var latitude = location.coordinates.lat;
+    var longitude = location.coordinates.long;
+    console.log('latitude', latitude)
+
+    return (
+        <IonPage>
+            <IonContent>
+                <IonTabButton id="share" onClick={GeoLocation}>
+                    Share My Location
+                    <br></br>
+                    latitude {latitude}
+                    <br></br>
+                    Longitude: {longitude}
+                    
+                </IonTabButton>
+            </IonContent>
+        </IonPage>
+    );
+
 }
 
 export default Map;
--- nextjs-tailwind-ionic-capacitor-starter/components/pages/nav.js
+++ nextjs-tailwind-ionic-capacitor-starter/components/pages/nav.js
@@ -1,5 +1,4 @@
 
-import { IonPage, IonContent, IonTabButton } from '@ionic/react';
 // // check if location services is turned on
 // // if location servies are allowed we fetch for user longitude and latitude
 // // else if location services doesn't work or the user declines the program wont run
@@ -15,16 +14,36 @@ import { useEffect, useState } from 'react';
 //     });
 // };
 
-// export default handleClick;
+// export default handleClick; 
+
+// AIzaSyClcFS24hcWrXCA0PciBNd97GpvZyednvY
+
+// calback to the inbuilt function innit map
+// google maps function
+
+// function initMap(){
+
+//     var settings = {
+//         zoom: 4,
+//         center: location
+//     }
+//     var map  = new google.maps.Map(document.getElementById('map'), settings);
+// }
+
+// intitMap();
 
 
+// linking api key
+//https://maps.googleapis.com/maps/api/js?key=AIzaSyClcFS24hcWrXCA0PciBNd97GpvZyednvY&callback=initMap";
+
+// opening it in google maps
 
 
 
 
 const GeoLocation = () => {
 
-
+    console.log(1)
     const [location, setLocation] = useState({
         coordinates : {lat: '', long: ''},
         status: null
@@ -32,9 +51,9 @@ const GeoLocation = () => {
     });
 
     function GetPosition(location){
-            
-        var latitude = location.coords.latitude;
-        var longitude = location.coords.longitude;
+        console.log(2)
+        const latitude = location.coords.latitude;
+        const longitude = location.coords.longitude;
         setLocation({
             coordinates: {
                 lat: latitude, 
@@ -42,9 +61,15 @@ const GeoLocation = () => {
             },
             status: 1
         });
+        
+
+
+
 
         console.log('location: ', latitude, longitude);
-        console.log('success')
+        console.log('success');
+
+        return location;
             
     }
 
@@ -55,25 +80,31 @@ const GeoLocation = () => {
         })
 
     }
-
+    console.log(3)
     useEffect(() => {
 
-
+        console.log(4)
         const handleClick = () => {
 
             if(!('geolocation' in navigator)){
+                console.log('100100000')
                 ErrorHandler('Unable to access geolocation services');
             }
             else{
+                console.log('loading')
                 navigator.geolocation.getCurrentPosition(GetPosition, ErrorHandler);
             }
             
 
         };
         
+        console.log()
         document.querySelector("#share").addEventListener("click", handleClick);
     
     }, []);
+
+    return location;
+
 }
 
 
